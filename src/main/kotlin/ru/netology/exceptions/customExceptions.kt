88d1ec086b671545dfcce68fa390357c8758a2db
rainman-wf <denviva@gmail.com>
2022-04-10 20:11:26 +0300

    #3.2 collections - tests 95%

--- src/main/kotlin/ru/netology/exceptions/customExceptions.kt
+++ src/main/kotlin/ru/netology/exceptions/customExceptions.kt
@@ -1,6 +1,8 @@
 package ru.netology.exceptions
 
-class PostNotFoundException : NullPointerException("Post not found")
-class CommentNotFoundException : RuntimeException("Comment not found")
-class InvalidReasonCodeException : RuntimeException("Invalid report reason")
-class NoteNoteFoundException: NullPointerException("Note not found")
+class CommentNotFoundException : NullPointerException("Comment not found")
+class NoteNoteFoundException : NullPointerException("Note not found")
+class AccessDeniedException : RuntimeException("Access denied")
+class CommentAlreadyDeletedException : RuntimeException("Comment already deleted")
+class NothingToRestoreException : RuntimeException("Comment is not deleted")
+class InvalidUserIdException: NullPointerException("Invalid user Id")
